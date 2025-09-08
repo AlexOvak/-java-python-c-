7ad2a9bb29ad65ad63de74665e529ffdc39e5bb8
AlexOvak <misterrrr69@gmail.com>
2025-09-08 19:23:49 +0300

    java script

---
+++ java_script.java
@@ -0,0 +1,20 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        {
+            int nums[]; //объявление массива с названием и типом
+            nums = new int[3]; //инициализация массива с определением кол-ва чисел
+
+            int[] nums2 = { 1, 2, 3, 5 };
+            nums[0] = 1; //установка значений в массиве
+            nums[1] = 2;
+            nums[2] = 3;
+
+            System.out.println(nums[0]);
+            System.out.println(nums2[3]);
+
+
+        }
+    }
+}
